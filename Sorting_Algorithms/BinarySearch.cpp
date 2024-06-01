ffc2fb1495215a118bc824596a71ba0d1821c496
NarekYedigaryan <yedigaryan.narek04@gmail.com>
2024-06-01 21:42:32 +0400

    some additions in insertion sort and binary search

---
+++ Sorting_Algorithms/BinarySearch.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+int BinarySearch_iterative(int*,int,int);
+int BinarySearch_recursive(int*,int,int,int,int);
+int main()
+{
+   int arr[7] = {1,2,3,4,5,6,7}; 
+
+//  std::cout<<BinarySearch_iterative(arr,7,9);
+   std::cout<<BinarySearch_recursive(arr,7,9,0,6);
+
+}
+
+int BinarySearch_iterative(int* arr,int size,int target)
+{
+  int start = 0;
+  int end = size - 1;
+  int mid = 0;
+  while(start <= end)
+  {
+    mid = start + (end-start)/2;
+    if(target == arr[mid])
+    {
+        return mid;
+    }
+    else if(arr[mid] <= target)
+    {
+        start = mid + 1;
+    }
+    else
+    {
+        end = mid - 1;
+    }
+  }
+  return -1;
+}
+
+int BinarySearch_recursive(int* arr,int size,int target,int start,int end)
+{
+    int mid = start + (end-start)/2;
+    if(start>end)
+    {
+        return -1;
+    }
+    if(target == arr[mid])
+    {
+        return mid;
+    }
+    else if(arr[mid] <= target)
+    {
+        BinarySearch_recursive(arr,size,target,mid+1,end);
+    }
+    else
+    {
+        BinarySearch_recursive(arr,size,target,start,mid-1);
+    }
+}
---
+++ Sorting_Algorithms/InsertionSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+void InsertionSort(int*,int);
+
+int main()
+{
+   const int n = 7;
+   int arr[n] = {1,9,-5,3,55,66,7};
+   // std::sort(arr,arr+n);
+   // for (size_t i = 0; i < n; i++)
+   // {
+   //   std::cout<<arr[i]<<" ";
+   // }
+   
+    InsertionSort(arr,n);
+}
+
+void InsertionSort(int* arr,int size)
+{
+   int j = 0; 
+   int result = 0;
+   for (size_t i = 1; i < size; ++i)
+   {
+      result = arr[i];
+      j=i-1;
+      while(j>=0 and arr[j]>result)
+      {
+        arr[j+1] = arr[j];
+        --j;
+      }
+      arr[j+1] = result;
+   }
+   for (size_t i = 0; i < size; i++)
+   {
+    std::cout<<arr[i]<<" ";
+   }
+   
+}
